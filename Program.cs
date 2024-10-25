6a8ba8d5997fb0f10f1286e9fab0fab8de65603f
sedef <sedef@SEDEF>
2024-10-25 20:53:07 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yazilim251002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir sayı girin");
+            int sayi=Convert.ToInt32(Console.ReadLine());
+            if (sayi%2==0 )
+            {
+                Console.WriteLine("çift sayı");
+            }
+            else
+            {
+                Console.WriteLine("tek sayı");
+            }
+            Console.ReadKey();
+        }
+    }
+}
